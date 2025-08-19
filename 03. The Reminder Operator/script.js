0ed1767e6b03fd2ce7c3f29050bcb9e98d6a7bef
ephraim <ephraims.818@gmail.com>
2025-08-19 09:54:00 +0600

    JS--COMPLETE-JS---NUMBERS-DATES-INTL-TIMERS - 03. The Reminder Operator

---
+++ 03. The Reminder Operator/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+console.log(5 % 2);
+console.log(5 / 2);
+console.log(8 % 3);
+console.log(8 / 3);
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(1));
+console.log(isEven(2));
+console.log(isEven(10));
+console.log(isEven(11));
+
